25cc2ee6023860e0c4264c42c233bda78a4fad2c
DPNA <dpnagames@gmail.com>
2026-01-28 16:31:46 -0300

    Atualizei para uma versão melhorada (brasileirão.js)
    Adicionei melhorias ao código, corrigindo erros direcionados às setas.

--- brasileirao.js
+++ brasileirao.js
@@ -1,24 +1,27 @@
-let rodadaAtual = 2; // Altere este valor para definir a rodada inicial a ser exibida
+let rodadaAtual = 1; // Altere este valor para definir a rodada inicial a ser exibida
 const rodadaMin = 1;
 const rodadaMax = 38;
 
 async function atualizarJogos() {
 
     if (rodadaAtual === 1) {
-        btnAnterior.style.visibility = "hidden"; 
-    } else if (rodadaAtual === 38) {
-        btnProximo.style.visibility = "hidden"; 
+        btnAnterior.style.visibility = "hidden";
+    } else if (rodadaAtual >= 2 && rodadaAtual < rodadaMax) {
+        btnProximo.style.visibility = "visible";
+        btnAnterior.style.visibility = "visible";
+    } else if (rodadaAtual === rodadaMax) {
+        btnProximo.style.visibility = "hidden";
     }
-    
+
     try {
-        const response = await fetch('/DPNA FutGame/imagens FutGame/jogosBR.json'); // Arquivo JSON local com os jogos
+        const response = await fetch('jogosBR.json'); // Arquivo JSON local com os jogos
         const jogos = await response.json();
 
         const listaJogos = document.getElementById('lista-jogos');
         listaJogos.innerHTML = '';
 
         const jogosDaRodada = jogos.filter(jogo => jogo.rodada === rodadaAtual);
-        
+
         jogosDaRodada.forEach(jogo => {
             const item = document.createElement('li');
             item.innerHTML = `
